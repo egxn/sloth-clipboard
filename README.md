ebd14b4e1c83e2831e5772bc4201f4be1a0a25d1
egxn <afvelosas@correo.udistrital.edu.co>
2022-09-19 19:59:00 -0500

    🦥 Add UI library

---
+++ README.md
@@ -0,0 +1,11 @@
+# Sloth 🦥
+
+Clipboard manager.
+
+# Built with:
+
+- [Rust](https://www.rust-lang.org/)
+- [egui](https://github.com/emilk/egui) 
+- [eframe](https://github.com/emilk/egui/tree/master/crates/eframe)
+- [arboard](https://github.com/1Password/arboard)
+
--- src/main.rs
+++ src/main.rs
@@ -1,68 +1,54 @@
-use arboard::Clipboard;
-use hyper::{Body, Request, Response, Server, Method, StatusCode};
-use hyper::service::{make_service_fn, service_fn};
-use local_ip_address::local_ip;
-use std::convert::Infallible;
-use std::net::SocketAddr;
-use tokio::{fs, task, time};
-use tokio::io::AsyncReadExt;
-use tokio::io::AsyncWriteExt;
+use arboard::{Clipboard, Error as ClipboardError};
+use eframe::{egui, NativeOptions};
 
-async fn routes(req: Request<Body>) -> Result<Response<Body>, Infallible> {
-    let response;
-  
-    match (req.method(), req.uri().path()) {
-      (&Method::GET, "/") => response = Response::new(Body::from("Clipboard Up")),
-      (&Method::GET, "/cp") => {
-        let mut file = fs::File::open("cp").await.unwrap();
-        let mut contents = vec![];
-        file.read_to_end(&mut contents).await.unwrap();
-        let content = String::from_utf8(contents).unwrap();
-        let body: String = std::fs::read_to_string("./src/template").unwrap().parse().unwrap();
-        response = Response::builder().status(StatusCode::OK)
-                                    .header("Content-Type", "text/html")
-                                    .body(Body::from(body.replace("{{content}}", &content)))
-                                    .unwrap();
-      },
-      (_,_) => response = Response::builder()
-        .status(StatusCode::NOT_FOUND)
-        .body(Body::from("Not Found"))
-        .unwrap(),
-    }
-    
-    Ok(response)
-  }
+fn main() {   
+    let options: NativeOptions = eframe::NativeOptions::default();
 
-#[tokio::main]
-async fn main() {    
-    task::spawn(async move {
-        let mut clipboard = Clipboard::new().unwrap();
-        let mut prev_text = clipboard.get_text().unwrap();
-        let mut file = fs::OpenOptions::new()
-            .write(true)
-            .create(true)
-            .append(true)
-            .open("cp")
-            .await
-            .unwrap();
+    eframe::run_native(
+        "Sloth 🦥",
+        options,
+        Box::new(|_cc| Box::new(MyApp::default())),
+    );
+}
 
-        loop {
-            let text = clipboard.get_text().unwrap();
-            if text != prev_text {
-                prev_text = text.clone();
-                file.write_all(text.as_bytes()).await.unwrap();
-            }
-            time::sleep(time::Duration::from_millis(400)).await;
+
+struct MyApp {
+    clips: Vec<String>,
+}
+
+impl Default for MyApp {
+    fn default() -> Self {
+        Self {
+            clips: Vec::new(),
         }
-    });
+    }
+}
 
+impl eframe::App for MyApp {
+    fn clear_color(&self, _visuals: &egui::Visuals) -> egui::Rgba {
+        egui::Rgba::TRANSPARENT
+    }
 
-    let addr = SocketAddr::from((local_ip().unwrap(), 1312));
-    let make_svc = make_service_fn(|_conn| async { Ok::<_, Infallible>(service_fn(routes)) });
-    let server = Server::bind(&addr).serve(make_svc);
-    println!("Up on http://{}", addr);
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+  
 
-    if let Err(e) = server.await {
-        eprintln!("server error: {}", e);
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.set_width(300.0);
+            ui.set_height(ui.available_height());
+            ui.vertical(|ui| {
+                self.clips.iter().for_each(|clip| {
+                    ui.label(clip);
+                });
+            });
+
+            let mut clipboard: Clipboard = Clipboard::new().unwrap();
+            let text:Result<String, ClipboardError> = clipboard.get_text();
+            if let Ok(text ) = text {
+                let last: Option<&String> = self.clips.last();
+                if last.is_none() || last.unwrap() != &text {
+                    self.clips.push(text);
+                }
+            }
+        });
     }
 }
